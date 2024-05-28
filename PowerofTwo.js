49dd5879c34d61d7b7c7241b272e71864ef1f964
Jeth_Dev <jethrosumbeiywet@gmail.com>
2024-05-28 11:33:51 +0300

    f

--- PowerofTwo.js
+++
@@ -1,9 +0,0 @@
-// Write a program that takes an integer as input and returns true if the input is a power of two
-function isPowerOfTwo(n) {
-    return n > 0 && (n & (n - 1)) === 0;
-}
-function power(integerInput) {
-    return isPowerOfTwo(integerInput);
-}
-var integerInput = 8;
-console.log(power(integerInput));
